b630c197598b5e6eee810bfdc3a3df8dcd437013
«Nadezhda <nadezhdagenna@mail.ru>
2022-08-27 01:46:45 +0300

    Task47 is finished.

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -4,33 +4,36 @@
 //1 - 3,3 8 - 9,9
 //8 7,8 - 7,1 9
 
-double [,] CreateMatrixRndDouble (int row, int col, int min, int max)
-{
-    double [,] matrix = new double [row, col];
-    Random rnd = new Random ();
+Console.WriteLine("Введите количество строк");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[lines, columns];
+System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-En");
+FillMatrixRandomNumbers(numbers);
+PrintMatrix(numbers);
+Random random = new Random();
 
+void FillMatrixRandomNumbers(double[,] matrix)
+{
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix [i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
         }
     }
-    return matrix;
 }
 void PrintMatrix(double[,] matrix)
 {
-
     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write("[ ");
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                Console.Write(matrix[i, j] + " ");
-            }
-            Console.Write("]");
-            Console.WriteLine("");
+            Console.Write($"{matrix[i,j]}   ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
     }
-double[,] array2D = CreateMatrixRndDouble(3, 4, -9, 9);
-PrintMatrix(array2D);
 }
