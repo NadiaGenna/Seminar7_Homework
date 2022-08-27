c3b47adc20f22e9092582ca93213ad8bdb5bdc74
«Nadezhda <nadezhdagenna@mail.ru>
2022-08-27 19:11:22 +0300

    Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -6,3 +6,33 @@
 //1, 7->такого элемента в массиве нет
 
 
+    Console.Write("Введите номер строки: ");
+    int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+    Console.Write("Введите номер столбца:  ");
+    int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+    int n = 5; // размер массива
+    int m = 7; // размер массива
+    Random random = new Random();
+    int[,] matrix = new int[n, m];
+    Console.WriteLine("Исходный массив:");
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(1, 10);
+            Console.Write("{0} ", matrix[i, j]);
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+    if (pos1 < 0 | pos1 > matrix.GetLength(0) - 1 | pos2 < 0 | pos2 > matrix.GetLength(1) - 1)
+    {
+        Console.WriteLine("Элемент не существует");
+    }
+    else
+    {
+        Console.WriteLine("Значение элемента массива = {0}", matrix[pos1, pos2]);
+    }
+    Console.ReadLine();
